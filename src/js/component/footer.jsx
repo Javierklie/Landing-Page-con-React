39026b30985ff1cac2043395da950f53d0786145
Javierklie <javierklie1@gmail.com>
2022-10-09 16:27:19 +0000

    Project

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,9 @@
+import React from "react"; //Main React.js library
+
+
+
+
+export const Footer = () => {
+	return (<div className="container-fluid bg-dark h-5 text-center p-3 text-white"><p>Copyright © Your Webpage 2022</p></div>)
+};
+
